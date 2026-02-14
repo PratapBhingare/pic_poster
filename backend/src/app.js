d1683eb2ac387f26c2122c9a1451ce9f8b1eb87f
PratapBhingare <pratapbhingare28@gmail.com>
2026-02-14 15:30:38 +0530

    changes in spp

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,35 +1,29 @@
-const app = require("../server");
-const connect = require("./db/db");
 const express = require("express");
-const postModel = require("../models/posts.model");
+const app = express();
+const postModel = require("./models/posts.model");
 
-connect();
-
-app.listen(3000, () => {
-    console.log("connected to port 3000")
-});
 
 app.use(express.json());
 
-app.get("/posts" , async (req,res) => {
-    const posts =  await postModel.find();
+app.get("/posts", async (req, res) => {
+  const posts = await postModel.find();
 
-    res.status(200).json({
-        message:"Here are all posts",
-        posts
-    })
+  res.status(200).json({
+    message: "Here are all posts",
+    posts,
+  });
 });
 
+app.post("/posts", async (req, res) => {
+  const post = await postModel.create({
+    image: req.body.image,
+    caption: req.body.caption,
+  });
 
-app.post("/posts" , async (req,res) => {
-    const post = await postModel.create({
-        image:req.body.image,
-        caption:req.body.caption
-    });
-
-    res.status(201).json({
-        message:"post created",
-        post
-    })
+  res.status(201).json({
+    message: "post created",
+    post,
+  });
 });
 
+module.exports = app;
